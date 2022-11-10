88054bde619d999a2fcd91535c5cb4874de16e3c
AlexR32 <58864658+AlexR32@users.noreply.github.com>
2022-11-10 17:22:35 +0300

    fix

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 repeat task.wait() until game.GameId ~= 0
 
-if Parvus and Parvus.Loaded then
+if Parvus and Parvus.Game then
     Parvus.Utilities.UI:Notification({
         Title = "🎃 Parvus Hub",
         Description = "Script already running!",
@@ -34,16 +34,16 @@ end
 
 local function GetScript(Script)
     return Parvus.Debug and readfile("Parvus/" .. Script .. ".lua")
-    or game:HttpGetAsync(("%s/%s.lua"):format(Parvus.Domain,Script))
+    or game:HttpGetAsync(("%s%s.lua"):format(Parvus.Domain,Script))
 end
 
 local function LoadScript(Script)
     return loadstring(Parvus.Debug and readfile("Parvus/" .. Script .. ".lua")
-    or game:HttpGetAsync(("%s/%s.lua"):format(Parvus.Domain,Script)))()
+    or game:HttpGetAsync(("%s%s.lua"):format(Parvus.Domain,Script)))()
 end
 
-getgenv().Parvus = {Domain = "https://raw.githubusercontent.com/AlexR32/Parvus/main",
-    Debug = LoadArgs[1],Game = "None",Loaded = false,Utilities = {},Games = {
+getgenv().Parvus = {Debug = LoadArgs[1],Utilities = {},
+    Domain = "https://raw.githubusercontent.com/AlexR32/Parvus/main/",Games = {
         ["Universal" ] = {Name = "Universal",                 Script = "Universal" },
         ["1168263273"] = {Name = "Bad Business",              Script = "Games/BB"  },
         ["1586272220"] = {Name = "Steel Titans",              Script = "Games/ST"  },
@@ -64,7 +64,7 @@ LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
         QueueOnTeleport(([[local LoadArgs = {%s}
         loadstring(LoadArgs[1] and readfile("Parvus/Loader.lua") or
-        game:HttpGetAsync("%s/Loader.lua"))(unpack(LoadArgs))
+        game:HttpGetAsync("%sLoader.lua"))(unpack(LoadArgs))
         ]]):format(Concat(LoadArgs,","),Parvus.Domain))
     end
 end)
@@ -76,5 +76,5 @@ if SupportedGame then
         Title = "🎃 Parvus Hub",
         Description = Parvus.Game .. " loaded!",
         Duration = LoadArgs[2]
-    }) Parvus.Loaded = true
+    })
 end
